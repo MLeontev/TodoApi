8bdee0cdab6812f48f2940743cdbe55fa3c77a46
Максим Леонтьев <max.leontiev@mail.ru>
2024-05-30 11:32:29 +0500

    Back-end

---
+++ TodoApi/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models;
+
+public class Category
+{
+    public long Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public List<TodoItem> TodoItems { get; set; }
+}
